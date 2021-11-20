2c26c2e83db434fc897d6ca73d41a433e1338a6f
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2021-11-20 16:58:19 +0200

    Creat noua clasa CasutaCandidat

---
+++ src/CasutaCandidat.java
@@ -0,0 +1,19 @@
+public class CasutaCandidat {
+
+    private String numeCandidat;
+    private String numePartid;
+    private String filepathLogo;
+
+    public String getNumeCandidat() {
+        return numeCandidat;
+    }
+
+    public String getNumePartid() {
+        return numePartid;
+    }
+
+    public String getFilepathLogo() {
+        return filepathLogo;
+    }
+
+}
