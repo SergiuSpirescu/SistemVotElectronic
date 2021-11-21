f53a29ed50f541d2792ea41709120623e4b8836b
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2021-11-21 13:15:39 +0200

    Creare schita fereastra principala

--- src/Start.java
+++ src/Start.java
@@ -1,8 +1,22 @@
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start extends Application {
 
     public static void main(String[] args) {
 
         System.out.println("Bun venit la vot!");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FereastraPrincipala.fxml"));
+        primaryStage.setTitle("Vot Electronic");
+        primaryStage.setScene(new Scene(root, 300 ,300));
+        primaryStage.show();
+    }
 }
