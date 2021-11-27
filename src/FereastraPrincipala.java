263cc8fb55f9698a9c7cb8d5300dd248a296fe9e
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2021-11-27 12:39:05 +0200

    Draft pentru Fereastra Principala > Pagina Start
    Se afiseaza ID sectie votare si buton pentru a incepe votarea. De implementat o metoda de Fetch din server pentru ID sectie cu toate datele SAU input de la utilizator

--- src/FereastraPrincipala.java
+++ src/FereastraPrincipala.java
@@ -1,4 +1,23 @@
-public class FereastraPrincipala {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FereastraPrincipala implements Initializable {
+
+
+    private int idSectieVot;
+    @FXML
+    private Label labelSectieVot = new Label("Bun venit la Sectia de votare");
+
+    public void setIdSectieVot(int idSectieVot) {
+        this.idSectieVot = idSectieVot;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        labelSectieVot.setText("Bun venit la Sectia de votare #" + Integer.toString(12345));
+    }
 }
