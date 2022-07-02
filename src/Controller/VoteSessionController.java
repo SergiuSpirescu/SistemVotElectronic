9bb8c8f0bd60b41759a6c51687deb3aa71873a85
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-07-02 16:27:56 +0300

    Modified all Views
    Added placeholder items to all views

--- src/Controller/VoteSessionController.java
+++ src/Controller/VoteSessionController.java
@@ -1,13 +1,33 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VoteSessionController {
     public void onExitButton(ActionEvent actionEvent) {
 
         System.out.println("Aplicatie oprita din buton . . . " + "Sesiune");
         System.exit(0);
+    }
+
+    public void switchToBallotView(ActionEvent event) throws IOException {
+        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+        Parent sessionRoot = FXMLLoader.load(getClass().getResource("../View/VoteBallotView.fxml"));
+        Scene sessionScene = new Scene(sessionRoot,screenSize.getWidth(), screenSize.getHeight());
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Vot Electronic");
+        stage.setMaximized(true);
+        stage.setScene(sessionScene);
+        stage.show();
     }
 }
