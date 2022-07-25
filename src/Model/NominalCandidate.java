d07e22b6fe393a3dcc1a20a5249a21b5c2b8f2b0
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-07-25 20:44:16 +0300

    Created first unit tests
    Added simple tests for NominalCandidate class, as part of the TDD Stragegy

--- src/Model/NominalCandidate.java
+++ src/Model/NominalCandidate.java
@@ -2,6 +2,9 @@ package Model;
 
 public class NominalCandidate extends BaseCandidate {
 
-    private int listSize = 1;
+    private final int listSize = 1;
 
+    public NominalCandidate() {
+
+    }
 }
---
+++ src/UnitTest/NominalCandidateTest.java
@@ -0,0 +1,37 @@
+package UnitTest;
+
+import Model.BaseCandidate;
+import Model.NominalCandidate;
+import org.junit.Assert;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class NominalCandidateTest extends BaseCandidate {
+
+    @Before
+    public void initValues() {
+
+    }
+
+    @org.junit.Test
+    public void testGetCandidateName() {
+        NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
+        assertEquals("Full Candidate Name", candidate.getCandidateName());
+    }
+
+    @org.junit.Test
+    public void testSetCandidateName() {
+        NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
+        candidate.setCandidateName("New Name");
+        assertEquals("New Name", candidate.getCandidateName());
+    }
+
+    @org.junit.Test
+    public void testGetPosition() {
+        NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
+        assertEquals(5,candidate.getPosition());
+
+        }
+    }
+}
