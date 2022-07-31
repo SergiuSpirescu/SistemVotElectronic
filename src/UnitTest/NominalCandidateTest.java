606488fb8303d66b55218e76661f0d0fa6db6eca
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-07-31 12:23:48 +0300

    Squashed commit of the following:
    commit b19d69bdf2429888c71b41ad34d4ce1f1fb00bb0
    Author: Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
    Date:   Sun Jul 31 12:22:42 2022 +0300
    
        Revert "Weird commit to sync the project"
    
        This reverts commit cb83908478754c47d5021bf822b368a16ce56577.
    
    commit b9d9a3c559d917f5ed3b536d9f06cef8dd497a40
    Author: Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
    Date:   Sun Jul 31 12:18:33 2022 +0300
    
        repair iml file
    
    commit 19f4fc016892624e556fc80b77579771ac04e0e8
    Author: Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
    Date:   Sun Jul 31 12:07:10 2022 +0300
    
        Revert "Merge branch 'main' into Work-Classes"
    
        This reverts commit 3cb393bdec1a6ba22dc88b142a8f7ba832aaf4b5, reversing
        changes made to cb83908478754c47d5021bf822b368a16ce56577.

--- src/UnitTest/NominalCandidateTest.java
+++ src/UnitTest/NominalCandidateTest.java
@@ -1,26 +1,31 @@
+package UnitTest;
+
 import Model.NominalCandidate;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NominalCandidateTest {
 
-    @org.junit.Test
+    @Test
     public void testGetCandidateName() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         assertEquals("Full Candidate Name", candidate.getCandidateName());
     }
 
-    @org.junit.Test
+    @Test
     public void testSetCandidateName() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         candidate.setCandidateName("New Name");
         assertEquals("New Name", candidate.getCandidateName());
     }
 
-    @org.junit.Test
+    @Test
     public void testGetPosition() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         assertEquals(5, candidate.getPosition());
 
     }
+
+
 }
