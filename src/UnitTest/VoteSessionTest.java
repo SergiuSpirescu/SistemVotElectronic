7fe60f168d00ba47f35a69ad98a194315c163203
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-07-31 13:11:09 +0300

    Created Test Class for VoteSession

---
+++ src/UnitTest/VoteSessionTest.java
@@ -0,0 +1,36 @@
+package UnitTest;
+
+import Model.VoteSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VoteSessionTest {
+
+    @Before
+    public void setUp() throws Exception {
+        static VoteSession testVoteSessionInstance = VoteSession.getInstance();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getSessionName() {
+    }
+
+    @Test
+    public void getPrecintID() {
+    }
+
+    @Test
+    public void setPrecintID() {
+    }
+}
