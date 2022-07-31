b19d69bdf2429888c71b41ad34d4ce1f1fb00bb0
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-07-31 12:22:42 +0300

    Revert "Weird commit to sync the project"
    This reverts commit cb83908478754c47d5021bf822b368a16ce56577.

--- src/UnitTest/NominalCandidateTest.java
+++ src/UnitTest/NominalCandidateTest.java
@@ -1,6 +1,5 @@
 package UnitTest;
 
-import Model.BaseCandidate;
 import Model.NominalCandidate;
 import org.junit.Test;
 
@@ -8,24 +7,25 @@ import static org.junit.Assert.*;
 
 public class NominalCandidateTest {
 
-    @org.junit.Test
+    @Test
     public void testGetCandidateName() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         assertEquals("Full Candidate Name", candidate.getCandidateName());
     }
 
-    @org.junit.Test
+    @Test
     public void testSetCandidateName() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         candidate.setCandidateName("New Name");
         assertEquals("New Name", candidate.getCandidateName());
     }
 
-    @org.junit.Test
+    @Test
     public void testGetPosition() {
         NominalCandidate candidate = new NominalCandidate("Full Candidate Name", 5, "Java Party");
         assertEquals(5, candidate.getPosition());
 
     }
+
 
 }
