106091e69a866ff9eab20dfefc4be70e553f1631
Marjacu <40730014+SergiuSpirescu@users.noreply.github.com>
2022-09-07 12:04:32 +0300

    Developing more in the parser

--- src/Model/ICandidate.java
+++ src/Model/ICandidate.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.util.List;
+
 interface ICandidate {
 
+    public void addCandidate(List<String> candidateList);
 }
--- src/Model/JSONHandler/JsonReader.java
+++ src/Model/JSONHandler/JsonReader.java
@@ -3,6 +3,7 @@ package Model.JSONHandler;
 
 import Model.Ballot;
 import Model.BaseCandidate;
+import Model.ListCandidate;
 import Model.VoteSession;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class JsonReader {
@@ -42,7 +44,33 @@ public class JsonReader {
            JsonNode ballotNode = mainNode.path("ballotList");
             final int numberOfBallots = mainNode.get("numberOfBallots").asInt();
 
+            if (ballotNode.isArray()) {
 
+                for (JsonNode jn : ballotNode) {
+                    JsonNode candidateNode = ballotNode.path("candidateList");
+
+                    if ((ballotNode.get("ballotType").asText().equals("List")) && (candidateNode.isArray())) {
+
+                        for (JsonNode cn : candidateNode) {
+                            ListCandidate bc = new ListCandidate() {};
+
+                            bc.setPositionCount(candidateNode.get("positionCount").asInt());
+                            bc.setPartyName(candidateNode.get("partyName").asText());
+                            JsonNode candidateArray = candidateNode.path("candidateList");
+
+                            if (candidateArray.isArray()) {
+
+                                ArrayList<String> ar = new ArrayList<>();
+                                ar = mainNode.path("ballotList").path("candidateList").asText();
+
+//                            session.getVoteBallots().add();
+                        }
+                        }
+                    } else {
+
+                    }
+                }
+            }
 
 
 
